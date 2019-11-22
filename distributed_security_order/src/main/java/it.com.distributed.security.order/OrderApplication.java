2effd9627f1ae137926a36722cd47f13151e9fb3
SanJingYe88 <822202281@qq.com>
2019-11-22 11:56:46 +0800

    Uaa授权服务的搭建,Order资源服务的搭建

---
+++ distributed_security_order/src/main/java/it.com.distributed.security.order/OrderApplication.java
@@ -0,0 +1,17 @@
+package it.com.distributed.security.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args){
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
---
+++ distributed_security_uaa/src/main/java/it/com/distributed/security/uaa/UaaApplication.java
@@ -0,0 +1,17 @@
+package it.com.distributed.security.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableHystrix
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UaaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UaaApplication.class,args);
+    }
+}
